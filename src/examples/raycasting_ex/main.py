2897a540ae275c0bb38793553e24b9e945076e9c
Roha Park <rohawolf@gmail.com>
2024-02-07 23:50:12 +0900

    add new player module

--- src/examples/raycasting_ex/main.py
+++ src/examples/raycasting_ex/main.py
@@ -3,6 +3,7 @@ import sys
 import pygame as pg
 
 from map import Map
+from player import Player
 from setting import GAME
 
 
@@ -12,19 +13,23 @@ class Game:
         self.cfg = cfg
         self.screen = pg.display.set_mode(self.cfg.RES)
         self.clock = pg.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
 
     def update(self):
+        self.player.update()
         pg.display.flip()
-        self.clock.tick(self.cfg.FPS)
+        self.delta_time = self.clock.tick(self.cfg.FPS)
         pg.display.set_caption(f"{self.clock.get_fps():.1f}")
 
     def draw(self):
         self.screen.fill('black')
         self.map.draw()
+        self.player.draw()
 
     def check_events(self):
         for event in pg.event.get():
---
+++ src/examples/raycasting_ex/player.py
@@ -0,0 +1,71 @@
+import math
+
+import pygame as pg
+
+from setting import PLAYER
+
+class Player:
+    def __init__(self, game, cfg=PLAYER) -> None:
+        self.game = game
+        self.cfg = cfg
+        self.x, self.y = self.cfg.POS
+        self.angle = self.cfg.ANGLE
+
+    def move(self) -> None:
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+
+        dx, dy, d_theta = 0, 0, 0
+        speed = self.cfg.SPEED * self.game.delta_time
+        rotation_speed = self.cfg.ROT_SPEED * self.game.delta_time
+
+        keys = pg.key.get_pressed()
+        # default key bindings
+        key_bindings = {
+            pg.K_w: {'translation': (cos_a, sin_a), 'rotation': 0},
+            pg.K_s: {'translation': (-cos_a, -sin_a), 'rotation': 0},
+            pg.K_a: {'translation': (sin_a, -cos_a), 'rotation': 0},
+            pg.K_d: {'translation': (-sin_a, cos_a), 'rotation': 0},
+            pg.K_LEFT: {'translation': (0, 0), 'rotation': -1},
+            pg.K_RIGHT: {'translation': (0, 0), 'rotation': 1},
+        }
+        for key, dynamics in key_bindings.items():
+            tr = dynamics['translation']
+            if keys[key]:
+                dx += tr[0] * speed
+                dy += tr[1] * speed
+                d_theta += dynamics['rotation'] * rotation_speed
+        
+        self.x += dx
+        self.y += dy
+        self.angle += d_theta
+        self.angle %= 2 * math.pi
+
+    def draw(self) -> None:
+        pg.draw.line(
+            self.game.screen,
+            'yellow',
+            tuple([100 * _ for _ in self.pos]),
+            (
+                self.x * 100 + self.game.cfg.WIDTH * math.cos(self.angle)
+                , self.y * 100 + self.game.cfg.WIDTH * math.sin(self.angle)
+            ),
+            2,
+        )
+        pg.draw.circle(
+            self.game.screen,
+            'green',
+            tuple([100 * _ for _ in self.pos]),
+            15,
+        )
+     
+    def update(self) -> None:
+        self.move()
+    
+    @property
+    def pos(self) -> tuple:
+        return (self.x, self.y)
+    
+    @property
+    def map_pos(self) -> tuple:
+        return (int(self.x), int(self.y))
